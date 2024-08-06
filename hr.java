12ca54689c256cfb9102a9452892caf612cafb1c
ruwan <isurujayasinghe7@gmail.com>
2024-08-06 12:50:31 +0000

    removed saman code

--- hr.java
+++ hr.java
@@ -1,5 +1,5 @@
 public static void main(String[] args) {
   System.out.println("Hello World");
-  System.out.println("Modified ruwan");
-  System.out.println("Modified Saman");
+  System.out.println("Modified ruwan 2");
+  
 }
