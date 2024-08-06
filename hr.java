a7be319903f03567ef453953528fc98ea0fb5f8b
ruwan <isurujayasinghe7@gmail.com>
2024-08-06 17:05:47 +0000

    int x=10

--- hr.java
+++ hr.java
@@ -5,6 +5,7 @@ public static void main(String[] args) {
   System.out.println("Modified ruwan 5");
   System.out.println("Modified ruwan 6");
   System.out.println("Modified ruwan 6");
+  int x= 10;
 
   
 }
