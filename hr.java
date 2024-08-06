e88a52e4669e0e013279b437e89a1eae3780cc61
ruwan <isurujayasinghe7@gmail.com>
2024-08-06 14:44:15 +0000

    Java hr 9

--- hr.java
+++ hr.java
@@ -3,5 +3,6 @@ public static void main(String[] args) {
   System.out.println("Modified ruwan 2");
   System.out.println("Modified ruwan 4");
   System.out.println("Modified ruwan 5");
+  System.out.println("Modified ruwan 6");
   
 }
